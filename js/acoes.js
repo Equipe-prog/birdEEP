43a4cd4f87064d93434ecc3aec4e6510a4f6cf74
Equipe-prog <131053965+Equipe-prog@users.noreply.github.com>
2023-04-17 18:48:40 -0300

    Add files via upload

---
+++ js/acoes.js
@@ -0,0 +1,17 @@
+"use strict"
+
+$("#sugestoes").click(function(){
+    window.location.href = "pages/sugestoForm.html";
+    console.log(1);
+
+})
+
+$("#back").click(function(){
+    window.location.href = "/index.html";
+    console.log("'");
+});
+$("#next").click(function(){
+    alert("adicionado");
+    window.location.href = "/index.html";
+    console.log("'");
+});
